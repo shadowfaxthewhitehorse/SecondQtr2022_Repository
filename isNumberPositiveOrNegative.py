df682144bd35fe8530004adf2aade84a08c0e036
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-05-30 10:23:52 +0530

    Update isNumberPositiveOrNegative.py

--- isNumberPositiveOrNegative.py
+++ isNumberPositiveOrNegative.py
@@ -4,9 +4,9 @@ inum = int(input("Please enter a number:"))
 
 if (inum > 0):
      print("The number is positive.")
-else if (inum < 0):
+elif (inum < 0):
      print("The number is negative.")
-else if(inum == 0):
+elif (inum == 0):
      print("The number is zero.")
 else:
      pass
