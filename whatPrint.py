69fb0d5408db44436b13ce1a75c51ab5913a8d85
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-05-30 11:23:05 +0530

    Update whatPrint.py

--- whatPrint.py
+++ whatPrint.py
@@ -1,4 +1,10 @@
 # Python question
 # 
-# what values will b successively take when this code is run?
+# what values will myval successively take when this code is run?
+
+myval = 1
+for a in range(1, 10, 2):
+    myval+ = (a + 2)
+print(myb)
+
 
