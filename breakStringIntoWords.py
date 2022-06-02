550c03135b956cc530f4145a5f24e1a533943442
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-06-02 15:43:40 +0530

    Update breakStringIntoWords.py

--- breakStringIntoWords.py
+++ breakStringIntoWords.py
@@ -1,3 +1,13 @@
 # Break up a string into its constituent words
 #
 #
+# PYTHON QUESTION.
+# Python question. How do I print a string one word at a time?
+#
+str = "Here is a riddle. What can run but not walk?"
+
+for word in str.split(" "):
+	word = word.strip("+-*/?.!")
+	print(word)
+
+#done!
