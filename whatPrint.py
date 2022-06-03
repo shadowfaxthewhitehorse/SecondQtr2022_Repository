d0c0ede391402db5a444bae07770eb395abc86de
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-06-03 13:46:18 +0530

    Update whatPrint.py

--- whatPrint.py
+++ whatPrint.py
@@ -1,10 +1,16 @@
 # Python question
 # 
-# what values will myval successively take when this code is run?
+# ESP:
+# Demando pri la programlingvo Python.
+#
+# What values will myval successively take when this code is run?
+# ESP:
+# Kiajn valorojn myval sinsekve prenos kiam ĉi tiu kodo estas rulita?
 
 myval = 1
 for a in range(1, 10, 2):
     myval+ = (a + 2)
+    
 print(myval)
 
 
