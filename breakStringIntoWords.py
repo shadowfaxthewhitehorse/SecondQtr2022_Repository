284b095b9237a9ca5175cf901766fc0222b57fd9
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-06-03 13:51:50 +0530

    Update breakStringIntoWords.py

--- breakStringIntoWords.py
+++ breakStringIntoWords.py
@@ -1,8 +1,15 @@
 # Break up a string into its constituent words
+# ESP:
+# Disrompu ŝnuron en ĝiajn konsistigajn vortojn.
 #
 #
 # PYTHON QUESTION.
-# Python question. How do I print a string one word at a time?
+# How do I print a string one word at a time?
+#
+# ESP:
+# Demandoj por Python-programistoj.
+# 
+# Kiel mi presi ĉenon unu vorton samtempe?
 #
 str = "Here is a riddle. What can run but not walk?"
 
