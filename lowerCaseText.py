e740ea02a20ba30d0931bdaed2d54aa79f7d1dd5
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-06-04 07:32:05 +0530

    Create lowerCaseText.py

---
+++ lowerCaseText.py
@@ -0,0 +1,14 @@
+#
+# PYTHON CODE QUESTION:
+#
+# Can you write Python code to turn text to lower case?
+# Input:  list of words (strings)
+#
+# Output: string in lower
+
+
+stringtxt = ["Welcome", "to", "Qwykrtech", "This","Is","JuSt","An","EXAMPLE"]
+
+stringtxt_lower = [x.lower() for x in stringtxt]
+
+print(stringtxt_lower)
