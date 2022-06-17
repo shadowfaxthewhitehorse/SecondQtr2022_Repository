c556f7a9398219e5daa65a399b10e6c25311c913
shadowfaxthewhitehorse <66239320+shadowfaxthewhitehorse@users.noreply.github.com>
2022-06-17 09:52:46 +0530

    Update yearAtOneHundred.py

--- yearAtOneHundred.py
+++ yearAtOneHundred.py
@@ -1,11 +1,26 @@
 # Python code: "Year you will turn 100" problem
 #
 # Spot the bug in the code
-#
+# Exercise:
 # Create a program that asks the user to enter their name and their age. 
 # Print out a message addressed to them that tells them the year that they will turn 100 years old. 
 # Note: for this exercise, the expectation is that you explicitly write out the year (and therefore be out of date the next year).
 #
+# From the following link: https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+#
+# ESPERANTO:
+#
+# Rimarku la cimon en la kodo.
+#
+# Kreu programon, kiu petas la uzanton enigi ilian nomon kaj ilian aĝon.
+#
+# Presu mesaĝon adresitan al ili, kiu diras al ili la jaron, kiam ili fariĝos 100 jarojn.
+#
+# Notu: por ĉi tiu ekzerco, la atendo estas, ke vi eksplicite skribu la jaron (kaj do estu malaktuala la venontan jaron).
+#
+# Ĉi tiu demando estas de la sekva ligo:
+#  https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
 
 
 def yearAt100():
